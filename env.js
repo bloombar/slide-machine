5940d4fdd047fa63c93ddc4d4892355a00643777
bloombar <2209098+bloombar@users.noreply.github.com>
2025-05-09 21:59:28 -0400

    better mobile layout

--- env.js
+++ env.js
@@ -7,7 +7,7 @@ const loadEnv = () => {
       'https://api.openai.com/v1/responses',
     OPENAI_MODEL: localStorage.getItem('openai-api-model') || 'gpt-4.1', //'gpt-3.5-turbo', //o4-mini
     OPENAI_HISTORY_DEPTH: localStorage.getItem('openai-history-depth') || 10,
-    PAUSE_BETWEEN_SLIDES: localStorage.getItem('pause-between-slides') || 12000,
+    PAUSE_BETWEEN_SLIDES: localStorage.getItem('pause-between-slides') || 10000,
     CLEAR_SUBTITLES_AFTER: localStorage.getItem('pause-between-slides') || 2000,
   }
 }
